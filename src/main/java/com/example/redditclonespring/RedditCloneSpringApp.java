f968c513ddc2b132928d379394d6209580d59813
IsraaAbdElnaby <israa.tobgy@gmail.com>
2022-11-16 09:23:53 +0200

    App Properties

---
+++ src/main/java/com/example/redditclonespring/RedditCloneSpringApp.java
@@ -0,0 +1,11 @@
+package com.example.redditclonespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditCloneSpringApp {
+    public static void main(String[] args) {
+        SpringApplication.run(RedditCloneSpringApp.class);
+    }
+}
